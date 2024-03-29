6de74b7b49f088493c8105e220b3d442d03efaf8
SWAYAM31220 <104613314+SWAYAM31220@users.noreply.github.com>
2024-03-29 14:57:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,17 @@ let getQuote = () => {
       author.innerText = item.author;
     });
 };
-
+ function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+ btn.onclick = function() {
+        const randomColor = getRandomColor();
+        document.body.style.backgroundColor = randomColor;
+    }
 window.addEventListener("load", getQuote);
 btn.addEventListener("click", getQuote);
