dd3948d7f7402a4a65072481e5c9a86c6c5bace8
SWAYAM31220 <104613314+SWAYAM31220@users.noreply.github.com>
2024-03-29 18:22:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let quote = document.getElementById("quote");
 let author = document.getElementById("author");
 let btn = document.getElementById("btn");
 const container = document.querySelector(".container");
+const quoteContainer = document.querySelector(".container p");
 const url = "https://api.quotable.io/random";
 
 let getQuote = () => {
@@ -12,14 +13,12 @@ let getQuote = () => {
       author.innerText = item.author;
     });
 };
- function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
+function copyText() {
+    const quoteText = quoteContainer.textContent;
+    navigator.clipboard.writeText(quoteText);
+}
+
+copyIcon.addEventListener("click", copyText);
 
 window.addEventListener("load", getQuote);
 btn.addEventListener("click", getQuote);
